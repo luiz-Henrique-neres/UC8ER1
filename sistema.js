e59a456f6d9b104f5c57012b7c3135fa77a7af9d
luiz henriq <riq546@gmail.com>
2022-11-23 19:31:50 -0300

    Atualização

--- sistema.js
+++ sistema.js
@@ -1,12 +1,12 @@
-var pesopeca =200;
+var pesopeca = 200;
 
-if(pesopeca<100){
+if(pesopeca < 100) {
     console.log("a peça deve ter no minimo 100g")
 }else{  
-    console.log("a peca possui peso adequado")
+    console.log("a peça possui peso adequado")
 }
 
-var listadepecas = ["Amortecedor","Motor", "Filtro de Ar", "Disco de Freio"]
+var listadepecas = ["Amortecedor", "Motor", "Filtro de Ar", "Disco de Freio"]
 
 if (listadepecas.length < 10) {
     console.log ("É possível cadastrar mais peças")
@@ -16,9 +16,9 @@ if (listadepecas.length < 10) {
 
 var nomepeca = "volante"
 
-if (nomepeca.length > 3){
+if (nomepeca.length > 3) {
     console.log("Nome da peça está adequado para o cadastro!")
-}else if (nomepeca.length == 0){
+}else if (nomepeca.length == 0) {
     console.log("O nome da peça não pode ser vazio!")
 }else{
     console.log("O nome inválido. Cadastre un nome correto!")
@@ -31,8 +31,8 @@ if (nomepeca.length > 3){
 // if (idade === 20);
 
 
-switch (nomepeca.length){
-    case 0 :
+switch (nomepeca.length) {
+    case 0:
         console.log("O nome da peça não pode ser vazio!");
         break;
         case 1:
